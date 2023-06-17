deba59c26d7a29d73e2fe2c15ff4b743049c1824
Robby6Strings <rausten93@gmail.com>
2023-06-18 07:09:29 +1200

    card-list now wrapped in 'list-container'

--- src/components/poll/PollList.tsx
+++ src/components/poll/PollList.tsx
@@ -6,17 +6,19 @@ import { PollCard } from "./PollCard"
 
 export const PollList = () => {
   return (
-    <Suspense promise={getPolls} cache prefetch:defer>
-      {(loading: boolean, items: PollData[]) => {
-        if (loading) return <div>Loading...</div>
-        return (
-          items?.length && (
-            <ul className="card-list">
-              <For each={items} template={(item) => <PollCard {...item} />} />
-            </ul>
+    <div className="list-container">
+      <Suspense promise={getPolls} cache prefetch:defer>
+        {(loading: boolean, items: PollData[]) => {
+          if (loading) return <i>Loading...</i>
+          return (
+            (items?.length && (
+              <ul className="card-list">
+                <For each={items} template={(item) => <PollCard {...item} />} />
+              </ul>
+            )) || <i>No polls found.</i>
           )
-        )
-      }}
-    </Suspense>
+        }}
+      </Suspense>
+    </div>
   )
 }
