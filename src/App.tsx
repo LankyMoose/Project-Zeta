13952a0fa6aac7b378b4589217435e3e42ddf0df
Robby6Strings <rausten93@gmail.com>
2023-06-20 22:10:45 +1200

    rmeove navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,18 @@
 import * as Cinnabun from "cinnabun"
-import { Router, Route, Link } from "cinnabun/router"
-import { pathStore } from "./state"
-import { UserList } from "./components/UserList"
 import { AuthLinks } from "./components/AuthLinks"
 import { Portal } from "./components/Portal"
 import { NotificationTray } from "./components/Notifications"
 import { PollList } from "./components/poll/PollList"
 import { PollCreator } from "./components/poll/PollCreator"
 
-const Navigation = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/users" store={pathStore}>
-          Users
-        </Link>
-      </li>
-      <li>
-        <AuthLinks />
-      </li>
-    </ul>
-  </nav>
-)
-
 const Header = () => {
   return (
     <header>
-      <Link to="/" store={pathStore}>
-        <div id="logo">Logo</div>
-      </Link>
+      <div id="logo">Logo</div>
 
       <div className="flex gap align-items-center">
         <PollCreator />
-        <Navigation />
+        <AuthLinks />
       </div>
     </header>
   )
@@ -43,10 +23,7 @@ export const App = () => {
     <>
       <Header />
       <main className="container">
-        <Router store={pathStore}>
-          <Route path="/" component={<PollList />} />
-          <Route path="/users" component={<UserList />} />
-        </Router>
+        <PollList />
       </main>
       <Portal>
         <NotificationTray />
