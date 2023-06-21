06fff368324d03a2d9587681539919c1d6fd1825
Robby6Strings <rausten93@gmail.com>
2023-06-21 21:14:12 +1200

    authLinks -> btn styles, btn style tweaks

--- src/components/AuthLinks.tsx
+++ src/components/AuthLinks.tsx
@@ -3,11 +3,21 @@ import { isAuthenticated, isNotAuthenticated, userStore } from "../state"
 
 export const AuthLinks = () => (
   <>
-    <a href="/login/google" watch={userStore} bind:visible={isNotAuthenticated}>
-      Log in with Google
+    <a
+      href="/login/google"
+      className="btn btn-primary hover-animate text-rg"
+      watch={userStore}
+      bind:visible={isNotAuthenticated}
+    >
+      <span>Log in with Google</span>
     </a>
-    <a href="/logout" watch={userStore} bind:visible={isAuthenticated}>
-      Log out
+    <a
+      href="/logout"
+      className="btn btn-secondary hover-animate text-rg"
+      watch={userStore}
+      bind:visible={isAuthenticated}
+    >
+      <span>Log out</span>
     </a>
   </>
 )
