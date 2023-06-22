b9c457cde57bb812a6e8e68d3e818dc4ea17b092
Robby6Strings <rausten93@gmail.com>
2023-06-22 20:58:11 +1200

    improve PollList implementation

--- src/components/poll/PollList.tsx
+++ src/components/poll/PollList.tsx
@@ -1,30 +1,24 @@
 import * as Cinnabun from "cinnabun"
-import { Cinnabun as cb, Signal } from "cinnabun"
+import { Cinnabun as cb, createSignal } from "cinnabun"
 import { For } from "cinnabun"
-import { PollData } from "../../types/polls"
 import { PollCard } from "./PollCard"
 import { LiveSocket } from "../../client/liveSocket"
 import { DefaultLoader } from "../loaders/Default"
 
 const isClient = cb.isClient
+
 export const PollList = () => {
-  const polls = isClient ? cb.getRuntimeService(LiveSocket).polls : []
+  const liveSocket = cb.getRuntimeService(LiveSocket)
+  const loading = isClient ? liveSocket.loading : createSignal(true)
+
+  const polls = isClient ? liveSocket.polls : []
 
   return (
     <div className="list-container">
-      <DefaultLoader
-        watch={polls}
-        bind:visible={() =>
-          !isClient ||
-          (isClient && (polls as Signal<PollData[]>).value.length === 0)
-        }
-      />
+      <DefaultLoader watch={loading} bind:visible={() => loading.value} />
 
       <ul className="card-list">
-        <For
-          each={isClient ? cb.getRuntimeService(LiveSocket).polls : []}
-          template={(item) => <PollCard {...item} />}
-        />
+        <For each={polls} template={(item) => <PollCard {...item} />} />
       </ul>
     </div>
   )
