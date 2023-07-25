ef0fa7e1663af07306b850a3390ea59520bb4f9c
Robby6Strings <rausten93@gmail.com>
2023-07-25 22:42:02 +1200

    update dev api url

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,6 +1,10 @@
 const isDev = process.env.NODE_ENV === "development"
 
-export const API_URL = isDev ? "http://localhost:3000/api" : "https://zetabase.xyz/api"
+export const API_URL = isDev
+  ? "http://localhost:3000/api"
+  : isDev
+  ? "https://project-zeta.up.railway.app/api"
+  : "https://zetabase.xyz/api"
 
 export enum API_ERROR {
   NOT_FOUND = "Not found",
