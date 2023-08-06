9cb7782a9f682ffda256c45522e4c714b3374722
Robby6Strings <rausten93@gmail.com>
2023-08-07 08:25:30 +1200

    Home page now uses PostCard from communityPage

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,15 +1,9 @@
 import * as Cinnabun from "cinnabun"
 import { getLatestPostsCommunities } from "../../client/actions/communities"
-import { pathStore } from "../state/global"
-import { selectedCommunity } from "../state/community"
-import { PostWithCommunityMeta } from "../../types/post"
-import { AuthorTag } from "../components/AuthorTag"
-import { Link } from "cinnabun/router"
-import { timeSinceUTCDate } from "../../utils"
+import { PostWithMeta } from "../../types/post"
 import { title } from "../Document"
-import { CommentIcon } from "../components/icons/CommentIcon"
-import { selectedPost } from "../state/post"
 import { SkeletonList } from "../components/loaders/SkeletonList"
+import { PostCard } from "../components/post/PostCard"
 
 export default function Home() {
   title.value = "Project Zeta"
@@ -20,7 +14,7 @@ export default function Home() {
       </div>
       <div className="page-body flex gap flex-wrap">
         <Cinnabun.Suspense promise={getLatestPostsCommunities} cache>
-          {(loading: boolean, data?: PostWithCommunityMeta[]) => {
+          {(loading: boolean, data?: PostWithMeta[]) => {
             if (loading)
               return <SkeletonList numberOfItems={3} height="140px" className="card-list w-100" />
             if (!data || !data.length)
@@ -32,61 +26,12 @@ export default function Home() {
             //return data.map((item) => <PostCard {...item} />)
             return (
               <ul className="card-list w-100">
-                <Cinnabun.For each={data} template={(item) => <PostCard {...item} />} />
+                <Cinnabun.For each={data} template={(item) => <PostCard post={item} />} />
               </ul>
             )
           }}
         </Cinnabun.Suspense>
       </div>
     </>
-  )
-}
-
-const PostCard = ({ post, community, user }: PostWithCommunityMeta) => {
-  const viewPost = () => {
-    selectedPost.value = {
-      ...post,
-      createdAt: new Date(post.createdAt),
-    }
-    window.history.pushState(null, "", `${window.location.pathname}?post=${post.id}`)
-  }
-
-  return (
-    <div className="card" key={post.id}>
-      <div className="card-title gap-lg flex justify-content-between">
-        <h4 className="m-0 title flex-grow w-100">
-          <a href="javascript:void(0)" onclick={viewPost}>
-            {post.title}
-          </a>
-        </h4>
-        <small className="text-right">
-          <Link
-            onBeforeNavigate={() => {
-              selectedCommunity.value = { ...community }
-              return true
-            }}
-            store={pathStore}
-            to={`/communities/${community.url_title}`}
-            className="text-primary nowrap"
-          >
-            {community.title}
-          </Link>
-        </small>
-      </div>
-      <div className="card-body">{post.content}</div>
-      <div className="flex justify-content-between align-items-end mt-auto">
-        <div>
-          <button type="button" className="btn text-sm p-0" onclick={viewPost}>
-            <span className="text-muted flex gap-sm align-items-center justify-content-center">
-              <CommentIcon />
-              {post.totalComments ?? 0}
-            </span>
-          </button>
-        </div>
-        <div className="flex flex-column align-items-end">
-          <AuthorTag user={user} date={timeSinceUTCDate(post.createdAt)} />
-        </div>
-      </div>
-    </div>
   )
 }
